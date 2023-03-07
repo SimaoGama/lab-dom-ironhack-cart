ce06d85d2e9ff0e9b23f84bb40a671f8eee915db
Simao Gama <gama.simao@gmail.com>
2023-03-07 23:57:56 +0000

    bonus missing

--- src/index.js
+++ src/index.js
@@ -3,21 +3,44 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+ const price = product.querySelector(".price span")
+ const quantity = product.querySelector(".quantity input")
+ let subtotal = product.querySelector(".subtotal span")
+
+ console.log(quantity.value) //loging to see the value amount
+
+ let result = subtotal.innerHTML = price.innerHTML * quantity.value;
+
+ return result; // return de forma a poder usar os valores na proxima função
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const products = document.querySelectorAll(".product")
+  const totalPrice = document.querySelector("#total-value span")
 
+  // const productsArray = [...products] fazer spread se usar o getElementById que retorna uma HTML collection
+  // console.log(productsArray)
+
+  let grandTotal = 0;
+  
+  products.forEach(product => {
+    let productSubtotal = updateSubtotal(product)
+    grandTotal += productSubtotal;
+  });
+
+  
   // ITERATION 3
-  //... your code goes here
+
+  totalPrice.innerHTML = grandTotal;
+
 }
 
 // ITERATION 4
